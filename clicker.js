d225a6f9a7dbc8ac02c1d0e40f9b56875ffd1f2f
Sergey Semushin <kezzyhko@gmail.com>
2022-03-15 01:39:34 +0300

    Refactor

--- clicker.js
+++ clicker.js
@@ -2,24 +2,35 @@ let clicks_counter = document.getElementById("clicks_counter")
 let click_button = document.getElementById("click_button")
 let upgrade_button = document.getElementById("upgrade_button")
 
-let value = 0
-let increasePerClick = 1
-let upgrade_price = 5
 
-function updateClicks(d) {
-	if (d < 0 && value < -d) { return false }
-	value += d
-	clicks_counter.innerText = value.toString()
+
+let gameInfo
+
+function resetGame() {
+    gameInfo = {}
+    gameInfo.value = 0
+    gameInfo.increasePerClick = 1
+    gameInfo.upgrade_price = 5
+    updateValue(0)
+}
+
+resetGame()
+
+
+
+function updateValue(d) {
+	if (d < 0 && gameInfo.value < -d) { return false }
+	gameInfo.value += d
+	clicks_counter.innerText = gameInfo.value.toString()
 	return true
 }
 
 click_button.addEventListener("click", function() {
-	updateClicks(increasePerClick)
+	updateValue(gameInfo.increasePerClick)
 })
 
 upgrade_button.addEventListener("click", function() {
-	let res = updateClicks(-upgrade_price)
+	let res = updateValue(-gameInfo.upgrade_price)
 	if (!res) { return }
-	increasePerClick += 1
+	gameInfo.increasePerClick += 1
 })
-
