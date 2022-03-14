f3bf74c28db4142121cd3bb33b350c800e31e5e2
Sergey Semushin <kezzyhko@gmail.com>
2022-03-14 17:15:13 +0300

    Add upgrade mechanics

--- clicker.js
+++ clicker.js
@@ -1,9 +1,25 @@
 let clicks_counter = document.getElementById("clicks_counter")
 let click_button = document.getElementById("click_button")
+let upgrade_button = document.getElementById("upgrade_button")
 
-let clicks = 0
+let value = 0
+let increasePerClick = 1
+let upgrade_price = 5
+
+function updateClicks(d) {
+	if (d < 0 && value < -d) { return false }
+	value += d
+	clicks_counter.innerText = value.toString()
+	return true
+}
 
 click_button.addEventListener("click", function() {
-	clicks++
-	clicks_counter.innerText = clicks.toString()
+	updateClicks(increasePerClick)
 })
+
+upgrade_button.addEventListener("click", function() {
+	let res = updateClicks(-upgrade_price)
+	if (!res) { return }
+	increasePerClick += 1
+})
+
